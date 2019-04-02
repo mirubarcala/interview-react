c97e21f0b01d9e73077bc8376d98cf83e7b11a07
Barcala <mbarcala@grupoassa.com>
2019-04-02 14:15:38 -0300

    segundo commit

--- src/views/Products.js
+++ src/views/Products.js
@@ -4,26 +4,54 @@ import { Link } from 'react-router-dom';
 
 
 class Products extends Component {
-  state = {
-    products: []
+  constructor(props){
+    super(props);
+  this.state = {
+    products: [],
+    searchString: ""
   }
+  this.handleChange = this.handleChange.bind(this);
+}
 
   async componentDidMount() {
     const response = await fetch('http://localhost:4180/products')
     const products = await response.json()
     this.setState({ products })
+    this.refs.search.focus();
   }
+
+  handleChange(){
+    this.setState({
+      searchString: this.refs.search.value
+    });
+  }
+
 
   render() {
     const { products } = this.state
+    let search = this.state.searchString.trim().toLowerCase();
+    let _products = products 
+    if (search.length > 0){
+      _products = _products.filter(function(product){
+        return product.name.toLowerCase().match(search);
+      });
+    }
     return (
       <div className="products">
         <header className="productsHeader">
           <h1>OUR PRODUCTS</h1>
+
+          <input
+            type="text"
+            value={this.state.searchString}
+            ref="search"
+            onChange={this.handleChange}
+            placeholder="Search products"
+          />
           
         </header>
         <ul className="productCards">
-          {products.map(product =>
+          {_products.map(product =>
             <li> 
               <div className="product">
                 <h2>
